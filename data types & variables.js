03d132818853eab8c5b22c25babf6ba2eb7c609f
Arsan <155844600+arsansomuncu@users.noreply.github.com>
2026-08-28 20:28:53 +0300

    Enhance data types & variables examples
    Added variable assignments, basic math operations, and increment/decrement examples.

---
+++ data types & variables.js
@@ -0,0 +1,104 @@
+/* data types: 
+undefined, null, boolean, string, symbol, number and object
+*/
+
+var MyName = "Arsan"
+myName = 8
+let ourName = "arsansomuncu"
+const pi = 3.14
+
+/* variable assignment */ 
+var a;
+var b = 2;
+console.log(a) /* Null */
+a = 7
+console.log(a) /* 7 */ 
+
+var a = 9; /* initialized variable */
+
+
+/* concetanation */ 
+var a = 5;
+var b = 6;
+var c = "I am a";
+
+a = a + 1; /* 6 */
+b = b + 1; /* 7 */
+c = c + "String!"; /* I am a String! */
+
+
+/* basic math */
+var sum = 10 + 10;
+console.log(sum) /* 20 */
+ 
+var difference = 45-9;
+var product = 8*10;
+var quotient = 66/33;
+console.log(difference)
+console.log(product)
+console.log(quotient)
+
+
+/* increment & decrement */
+var myVar = 87;
+myVar++;
+var myVar = 11;
+myVar--;
+
+
+/* finding the remainder */
+var remainder;
+remainder = 11 % 3;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

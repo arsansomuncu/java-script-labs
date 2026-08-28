c63214ed52003946a69bdb3173ab1e724932d264
Arsan <155844600+arsansomuncu@users.noreply.github.com>
2026-08-28 21:35:28 +0300

    Add array examples and access methods in arrays.js

---
+++ arrays.js
@@ -0,0 +1,64 @@
+var myArray = ["john", 23];
+var myArray = ["quincy", 1];
+
+/* multidimensional array*/
+
+var ourArray = [["the universe", 42], ["everything", 101010]];
+var myArray = [["bulls", 23],["white socks", 45]];
+
+/* access array data */
+var ourArray = [50,60,70];
+var ourData = ourArray[0]; // equals 50
+console.log(ourData)
+
+// modify array data 
+var ourArray = [18,64,99];
+ourArray[1] = 45;
+console.log(ourArray) // 18,45,99
+
+// access multidimensional arrays
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12],13,14]];
+var myData = myArray[2][1];
+console.log(myData) // prints 8 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

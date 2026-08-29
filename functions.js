5ce12f2e910c32f65773ea69e1ba4614fb1b1928
Arsan <155844600+arsansomuncu@users.noreply.github.com>
2026-08-29 21:02:05 +0300

    Add reusable functions and demonstrate scopes

---
+++ functions.js
@@ -0,0 +1,89 @@
+/* reuability of functions */
+function ourReusableFunction() {
+console.log("heyya, world");
+}
+ourReuableFunction();
+
+
+// arguments
+function ourFunctionWithArgs(a,b) {
+  console.log(a-b)
+} 
+ourFunctionWithArgs(10,5); // 5
+
+
+// global scope
+var myGlobal = 10;
+
+function fun1() {
+  oopsGlobal = 5;
+}
+
+function fun2() {
+var output = "";
+if (typeof myGlobal != "undefined") {
+  output += "myGlobal: " + myGlobal; 
+}
+  
+if (typeof oopsGlobal != "undefined") {
+  output += "oopsGlobal: " + oopsGlobal;
+}
+ console.log(output) 
+}
+
+fun1() // 5
+fun2() // 10
+
+
+// local scope
+function myLocalScope() {
+  var myVar = 5;
+  console.log(myVar) // 5
+}
+myLocalScope();
+console.log(myVar) // myVar is not defined.
+
+
+// return a value 
+function minusSeven(num){
+  return num - 7; 
+}
+console.log(minusSeven(10)) // 3
+
+function timesFive(num){
+  return num * 5;
+}
+console.log(timesFive(5)) // 25
+
+
+// undefined value returned
+var sum = 0;
+function addThree() {
+return sum += 3 ;
+}
+function addFive(){
+return sum += 5; // undefined
+}
+
+// assignment with a returned value 
+var changed = 0;
+function change(num) {
+return (num + 5) / 3; 
+}
+changed = change(10); // 5
+
+
+// stand in line
+function nextInLine(arr, item){
+  arr.push(item);
+  return arr.shift();
+}
+var testArr = [1,2,3,4,5];
+console.log("before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr,6));
+console.log("after: " + JSON.stringify(testArr));
+
+
+
+
+

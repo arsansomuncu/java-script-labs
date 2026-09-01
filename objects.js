751669cfb535d316d347af3c14285265a91c7d04
Arsan <155844600+arsansomuncu@users.noreply.github.com>
2026-09-01 14:33:04 +0300

    Add object definitions and property access examples

---
+++ objects.js
@@ -0,0 +1,62 @@
+var OurDog = {
+  "name" : "camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything"]
+};
+
+// dot notation
+
+var testObj{
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+var hatValue = testObj.hat;
+var shirtValue = testObj.shirt;
+
+// bracket notation
+
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+var hatValue = testObj["hat"];
+var shirtValue = testObj["shirt"];
+
+
+// variables in objects
+
+var testObj = {
+  12: "namath",
+  16: "montana",
+  19: "unitas"
+};
+
+var playerNumber = 16;
+var player = testObj[playerNumber];
+
+
+// updating object properties
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

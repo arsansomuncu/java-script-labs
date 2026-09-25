a270c82a38a779fd598fdd8c70aedec3d732e3fe
Arsan <155844600+arsansomuncu@users.noreply.github.com>
2026-09-25 14:43:49 +0300

    Enhance object manipulation examples in objects.js
    Updated object properties and added new examples for object manipulation, including adding and deleting properties, lookups, and nested objects.

--- objects.js
+++ objects.js
@@ -40,14 +40,79 @@ var player = testObj[playerNumber];
 
 // updating object properties
 
+var ourDog = {
+  "name": "camper",
+  "legs": 4,
+  "trails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.name = "happy camper"; // name: happy camper
+
+// add new properties to object 
+var ourDog = {
+  "name": "camper",
+  "legs": 4,
+  "trails": 1,
+  "friends": ["everything!"]
+};
+ourDog.bark = "bow-wow"; // "bark": "bow-wow"
+
+delete ourDog.bark; // delete properties from the object
 
 
+// objects for lookups
+function phoneticLookup(val){
+  var result = "";
+
+  var lookup {
+    "alpha": "adams",
+    "beta": "boston",
+    "gamma": "chicago",
+    "delta": "denver",
+    "echo": "easy"
+    };
+    
+    result = lookup[val];
+    return result;
+
+    console.log(phoneticLookup("gamma")); // chicago
+    
+}
 
 
+// testing objects for properties
+var myObj = {
+  gift: "ponny",
+  pet: "kitten",
+  bed: "sleigh"
+};
+
+function checkObj(checkProp){
+  if(myObj.hasOwnProperty(checkProp)){
+    return myObj[checkProp];
+  }
+  else{
+    return "not found";
+  }
+}
 
 
+// nested objects
+var myStorage = {
+  "car":{
+    "inside":{
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+    },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+}
 
-
+var gloveBoxContents = myStorage.car.inside["glove box"] 
+console.log(gloveBoxContents) // maps
 
 
 
